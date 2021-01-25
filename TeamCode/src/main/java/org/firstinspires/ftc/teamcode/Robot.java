01ed627cb7b014dc5a553fe410121ae7e19bac9c
Fishboy279 <APesce@Xavierhighschool.org>
2021-01-25 08:18:19 -0500

    Added in slow mode and reverse to the intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -50,6 +50,7 @@ public class Robot extends LinearOpMode {
         while (opModeIsActive()) {
 
             telemetry.addData("Status", "Running");
+            telemetry.addData("Wobble Encoder Value", wobbleMotor.getCurrentPosition());
             telemetry.addData("Shooter Velocity", shooter.getVelocity());
             telemetry.update();
 
@@ -60,22 +61,44 @@ public class Robot extends LinearOpMode {
             if (Math.abs(powerForward) < 0.05) powerForward = 0;
             double powerTurn = gamepad1.right_stick_x;
             if (Math.abs(powerTurn) < 0.05) powerTurn = 0;
-            double p1 = -powerStrafe + powerForward - powerTurn;
-            double p2 = powerStrafe + powerForward - powerTurn;
-            double p3 = -powerStrafe + powerForward + powerTurn;
-            double p4 = powerStrafe + powerForward + powerTurn;
-            double max = Math.max(1.0, Math.abs(p1));
-            max = Math.max(max, Math.abs(p2));
-            max = Math.max(max, Math.abs(p3));
-            max = Math.max(max, Math.abs(p4));
-            p1 /= max;
-            p2 /= max;
-            p3 /= max;
-            p4 /= max;
-            backLeftMotor.setPower(p1*.8);
-            frontLeftMotor.setPower(p2*.8);
-            frontRightMotor.setPower(p3*.8);
-            backRightMotor.setPower(p4*.8);
+            //If the right trigger is held down, robot goes into slow mode where it moves at 1/5 of full power compared to 4/5
+            if (gamepad1.left_trigger > .05) {
+                double p1 = -powerStrafe + powerForward - powerTurn;
+                double p2 = powerStrafe + powerForward - powerTurn;
+                double p3 = -powerStrafe + powerForward + powerTurn;
+                double p4 = powerStrafe + powerForward + powerTurn;
+                double max = Math.max(1.0, Math.abs(p1));
+                max = Math.max(max, Math.abs(p2));
+                max = Math.max(max, Math.abs(p3));
+                max = Math.max(max, Math.abs(p4));
+                p1 /= max;
+                p2 /= max;
+                p3 /= max;
+                p4 /= max;
+                backLeftMotor.setPower(p1 * .2);
+                frontLeftMotor.setPower(p2 * .2);
+                frontRightMotor.setPower(p3 * .2);
+                backRightMotor.setPower(p4 * .2);
+            } else {
+                //Allows the robot to move
+                double p1 = -powerStrafe + powerForward - powerTurn;
+                double p2 = powerStrafe + powerForward - powerTurn;
+                double p3 = -powerStrafe + powerForward + powerTurn;
+                double p4 = powerStrafe + powerForward + powerTurn;
+                double max = Math.max(1.0, Math.abs(p1));
+                max = Math.max(max, Math.abs(p2));
+                max = Math.max(max, Math.abs(p3));
+                max = Math.max(max, Math.abs(p4));
+                p1 /= max;
+                p2 /= max;
+                p3 /= max;
+                p4 /= max;
+                backLeftMotor.setPower(p1 * .8);
+                frontLeftMotor.setPower(p2 * .8);
+                frontRightMotor.setPower(p3 * .8);
+                backRightMotor.setPower(p4 * .8);
+            }
+
 
             if (gamepad2.left_bumper) {
                 wobbleMotor.setPower(-.40);
@@ -99,9 +122,12 @@ public class Robot extends LinearOpMode {
                 shooter.setPower(0);
             }
 
-            if (gamepad2.left_trigger > .05) {
+            if (gamepad2.right_trigger > .05) {
                 hopper.setPower(1);
                 intake.setPower(1);
+            } else if (gamepad2.left_trigger > .05) {
+                hopper.setPower(-1);
+                intake.setPower(-1);
             } else {
                 hopper.setPower(0);
                 intake.setPower(0);
